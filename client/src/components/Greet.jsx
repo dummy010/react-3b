a8cbc1042a1d7e03286a1c1be67d06ea630011f0
dummy010 <dummyaccoui0010@gmail.com>
2024-10-22 15:51:59 +0530

    hhh

--- client/src/components/Greet.jsx
+++ client/src/components/Greet.jsx
@@ -2,14 +2,20 @@
 
 import { useEffect, useState } from "react";
 import axios from "axios";
+// import Card from "./Card";
+import Card3 from "./Card";
 
 const Greet = () => {
   const [temp, setTemp] = useState(30);
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    axios("https://random.dog/woof.json").then((res) => setData(res.data));
-  }, [temp]);
+    axios("https://potterapi-fedeperin.vercel.app/en/characters").then((res) =>
+      setData(res.data)
+    );
+  });
+
+  // console.log(data);
 
   let temperature = 30;
   function chill() {
@@ -21,6 +27,14 @@ const Greet = () => {
     <div>
       <center>
         <h1>{temp} deg</h1>
+
+        {/* {console.log(data)} */}
+        <ul>
+          {data.map((ele) => (
+            <Card3 name={ele.fullName} img={ele.image} />
+          ))}
+        </ul>
+
         <img src={data.url} alt="" />
         <button onClick={chill}>Add Cooling</button>
       </center>
